c38bf555a22e325675658f837912a275a4108542
hamedfathi <noeffect@outlook.com>
2020-07-06 23:38:12 +0430

    init

---
+++ LoadSolutionForAnalysis/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Text;
+
+using static System.Console;
+
+namespace LoadSolutionForAnalysis
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Attempt to set the version of MSBuild.
+            VisualStudioInstance[] visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
+            VisualStudioInstance instance = visualStudioInstances.Length == 1
+                // If there is only one instance of MSBuild on this machine, set that as the one to use.
+                ? visualStudioInstances[0]
+                // Handle selecting the version of MSBuild you want to use.
+                : SelectVisualStudioInstance(visualStudioInstances);
+
+            WriteLine($"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
+
+            // NOTE: Be sure to register an instance with the MSBuildLocator 
+            //       before calling MSBuildWorkspace.Create()
+            //       otherwise, MSBuildWorkspace won't MEF compose.
+            MSBuildLocator.RegisterInstance(instance);
+
+            using MSBuildWorkspace workspace = MSBuildWorkspace.Create();
+            // Print message for WorkspaceFailed event to help diagnosing project load failures.
+            workspace.WorkspaceFailed += (o, e) => WriteLine(e.Diagnostic.Message);
+
+            string solutionPath = args[0];
+            WriteLine($"Loading solution '{solutionPath}'");
+
+            // Attach progress reporter so we print projects as they are loaded.
+            Solution solution = await workspace.OpenSolutionAsync(solutionPath, new ConsoleProgressReporter());
+            WriteLine($"Finished loading solution '{solutionPath}'");
+
+            // TODO: Do analysis on the projects in the loaded solution
+            foreach (Project project in solution.Projects)
+            {
+                WriteLine($"Project: {project.Name}");
+                foreach (Document document in project.Documents)
+                {
+                    WriteLine($"  - {document.Name}");
+                }
+            }
+        }
+
+        private static VisualStudioInstance SelectVisualStudioInstance(VisualStudioInstance[] visualStudioInstances)
+        {
+            WriteLine("Multiple installs of MSBuild detected please select one:");
+            for (int i = 0; i < visualStudioInstances.Length; i++)
+            {
+                WriteLine($"Instance {i + 1}");
+                WriteLine($"    Name: {visualStudioInstances[i].Name}");
+                WriteLine($"    Version: {visualStudioInstances[i].Version}");
+                WriteLine($"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
+            }
+
+            while (true)
+            {
+                var userResponse = ReadLine();
+                if (int.TryParse(userResponse, out int instanceNumber) &&
+                    instanceNumber > 0 &&
+                    instanceNumber <= visualStudioInstances.Length)
+                {
+                    return visualStudioInstances[instanceNumber - 1];
+                }
+                WriteLine("Input not accepted, try again.");
+            }
+        }
+
+        private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
+        {
+            public void Report(ProjectLoadProgress loadProgress)
+            {
+                var projectDisplay = Path.GetFileName(loadProgress.FilePath);
+                if (loadProgress.TargetFramework != null)
+                {
+                    projectDisplay += $" ({loadProgress.TargetFramework})";
+                }
+
+                WriteLine($"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LoadSolutionForAnalysis
+
+Based on this sample you can read & analyze other solutions by Roslyn.
+
+Run the following command in command-line:
+```bash
+LoadSolutionForAnalysis.exe "{YOUR_PATH}.sln"
+```
